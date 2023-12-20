16f62c6cdd61ab65e3214cce2bfcebcac709cb24
hien <hienle@freec.asia>
2023-12-20 10:21:51 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.screen.appBar}>
+        <Text style={styles.h1.center}> APLKKSA!!</Text>
+      </View>
+      <View style={styles.screen.container.row}>
+        <View style={[styles.screen.container.box, {backgroundColor: "powderblue"}]} />
+        <View style={[styles.screen.container.box, {backgroundColor: "skyblue"}]} />
+        <View style={[styles.screen.container.box, {backgroundColor: "steelblue"}]} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  screen: {
+    flex: 1, // same with weight
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    appBar: {
+      borderColor: "pink",
+      backgroundColor: "yellow",
+      height: 56,
+      justifyContent: "center",
+      marginTop: 36,
+      borderBottomWidth: 1,
+    },
+    container: {
+      flex: 1,
+      row: {
+        flexDirection: "row",
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+      },
+      box: {
+        width: 50,
+        height: 50,
+      },
+      backgroundBlack: {
+        flex: 1,
+        backgroundColor: "gray"
+      },
+      backgroundRed: {
+        flex: 2,
+        backgroundColor: "red"
+      },
+    }
   },
+  appBarTitle: {
+    color: "#000",
+    fontSize: 20
+  },
+  h1: {
+    fontSize: 22,
+    color: "#000",
+    center: {
+      alignItems: "center",
+    }
+  }
 });
